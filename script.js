5d22bc7dcda3809eff3cd25b86206806445b8fe3
starrixkis <bilqees.bnt.jam@gmail.com>
2025-01-13 19:19:37 +0100

    finished mobile

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ menu.addEventListener('click', function(event){
     if (clickTarget.classList.contains('nav-link')){
         clickTarget.classList.add('active');
         activeBtn.classList.remove('active');
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide');
+        }
+    }
+
+    let body = document.querySelector('body');
+    let mobileMenu = document.querySelector('.mobile-menu');
+    if (!body.classList.contains('off-scroll') &&
+            !mobileMenu.classList.contains('hide')){
+                body.classList.add('off-scroll');
+            }
+    else{
+        body.classList.remove('off-scroll')
     }
 });
 
